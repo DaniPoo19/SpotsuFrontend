9eb6ebaf7ca9cd4118863c5c587ac7851ed6a153
Daniel Negrete <Victor Negrete>
2025-05-13 19:31:58 -0500

    Actualizacion github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
@@ -17,4 +12,6 @@ export default defineConfig({
       '@': resolve(__dirname, './src'),
     },
   },
+  base: '/SpotsuFrontend/', // Reemplaza 'mi-proyecto' con el nombre de tu repositorio
 })
+
