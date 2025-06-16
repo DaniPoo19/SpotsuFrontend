b0b7725d6373926645a7384fe935b53c127ae0a4
Victor Negrete Baza <133527554+DaniPoo19@users.noreply.github.com>
2025-06-16 10:09:17 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,28 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: '/SpotsuFrontend/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
     },
   },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      },
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',
+        entryFileNames: 'assets/[name].[hash].js'
+      }
+    }
+  },
   server: {
     port: 5174,
     strictPort: true,
     host: true,
-  },
-  base: '/SpotsuFrontend/',
+  }
 });
-
