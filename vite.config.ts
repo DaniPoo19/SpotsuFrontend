1f601bd3f808f2d22de2e383f21546bf185d7563
Victor Negrete Baza <133527554+DaniPoo19@users.noreply.github.com>
2025-06-16 10:22:42 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,35 +4,20 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/SpotsuFrontend/', // Asegúrate que coincida exactamente con tu repositorio (case-sensitive)
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
     },
   },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    assetsInlineLimit: 0, // Fuerza a que los SVG no se conviertan en base64
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      },
-      output: {
-        assetFileNames: (assetInfo) => {
-          // Manejo especial para SVG
-          if (assetInfo.name?.endsWith('.svg')) {
-            return 'assets/[name][extname]'; // Sin hash para fácil referencia
-          }
-          return 'assets/[name].[hash][extname]';
-        },
-        entryFileNames: 'assets/[name].[hash].js'
-      }
-    }
-  },
   server: {
     port: 5174,
     strictPort: true,
     host: true,
-  }
+  },
+  base: '/',
 });
+
+
